4632ca173f0cdc3b60e745aa093f1b1570075477
JagmeetSinghGrewal5 <jsingh.27790.48@gmail.com>
2025-12-29 16:07:13 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3001', // Local development
-    'https://cipher-sql-studio-lovat.vercel.app', // Your frontend URL
+    'https://cipher-sql-studio-beige.vercel.app/',
+    'https://cipher-sql-studio-beige.vercel.app/ ',// Your frontend URL
   ],
   credentials: true
 }));
